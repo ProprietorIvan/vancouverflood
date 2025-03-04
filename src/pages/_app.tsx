16b71128774d9daa4ed36532d83e5fbfdca94488
ProprietorIvan <ivan.a.rogachyov@gmail.com>
2025-03-04 02:00:46 +0100

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,5 +3,10 @@ import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
